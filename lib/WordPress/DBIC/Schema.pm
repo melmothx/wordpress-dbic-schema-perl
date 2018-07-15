ad86bb06bb17c1d42d8f0d626e90c3657ce173bb
Marco Pessotto <melmothx@gmail.com>
2018-07-15 12:09:10 +0200

    Initial dbicdump against single site tables

--- lib/WordPress/DBIC/Schema.pm
+++ lib/WordPress/DBIC/Schema.pm
@@ -1,111 +1,20 @@
+use utf8;
 package WordPress::DBIC::Schema;
 
-use 5.010001;
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
 use strict;
 use warnings;
 
-=head1 NAME
+use base 'DBIx::Class::Schema';
 
-WordPress::DBIC::Schema - The great new WordPress::DBIC::Schema!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
+__PACKAGE__->load_namespaces;
 
 
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use WordPress::DBIC::Schema;
-
-    my $foo = WordPress::DBIC::Schema->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Marco Pessotto, C<< <melmothx at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-wordpress-dbic-schema at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=WordPress-DBIC-Schema>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+# Created by DBIx::Class::Schema::Loader v0.07046 @ 2018-07-15 12:06:53
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:DRt2wnm6VB6Nj529Ii7t8g
 
 
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc WordPress::DBIC::Schema
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=WordPress-DBIC-Schema>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/WordPress-DBIC-Schema>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/WordPress-DBIC-Schema>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/WordPress-DBIC-Schema>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2018 Marco Pessotto.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See L<http://dev.perl.org/licenses/> for more information.
-
-
-=cut
-
-1; # End of WordPress::DBIC::Schema
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
---
+++ lib/WordPress/DBIC/Schema/Result/WpPost.pm
@@ -0,0 +1,309 @@
+use utf8;
+package WordPress::DBIC::Schema::Result::WpPost;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+WordPress::DBIC::Schema::Result::WpPost
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 TABLE: C<wp_posts>
+
+=cut
+
+__PACKAGE__->table("wp_posts");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'bigint'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 post_author
+
+  data_type: 'bigint'
+  default_value: 0
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 post_date
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  default_value: '0000-00-00 00:00:00'
+  is_nullable: 0
+
+=head2 post_date_gmt
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  default_value: '0000-00-00 00:00:00'
+  is_nullable: 0
+
+=head2 post_content
+
+  data_type: 'longtext'
+  is_nullable: 0
+
+=head2 post_title
+
+  data_type: 'text'
+  is_nullable: 0
+
+=head2 post_excerpt
+
+  data_type: 'text'
+  is_nullable: 0
+
+=head2 post_status
+
+  data_type: 'varchar'
+  default_value: 'publish'
+  is_nullable: 0
+  size: 20
+
+=head2 comment_status
+
+  data_type: 'varchar'
+  default_value: 'open'
+  is_nullable: 0
+  size: 20
+
+=head2 ping_status
+
+  data_type: 'varchar'
+  default_value: 'open'
+  is_nullable: 0
+  size: 20
+
+=head2 post_password
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 post_name
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 200
+
+=head2 to_ping
+
+  data_type: 'text'
+  is_nullable: 0
+
+=head2 pinged
+
+  data_type: 'text'
+  is_nullable: 0
+
+=head2 post_modified
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  default_value: '0000-00-00 00:00:00'
+  is_nullable: 0
+
+=head2 post_modified_gmt
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  default_value: '0000-00-00 00:00:00'
+  is_nullable: 0
+
+=head2 post_content_filtered
+
+  data_type: 'longtext'
+  is_nullable: 0
+
+=head2 post_parent
+
+  data_type: 'bigint'
+  default_value: 0
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 guid
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 menu_order
+
+  data_type: 'integer'
+  default_value: 0
+  is_nullable: 0
+
+=head2 post_type
+
+  data_type: 'varchar'
+  default_value: 'post'
+  is_nullable: 0
+  size: 20
+
+=head2 post_mime_type
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 100
+
+=head2 comment_count
+
+  data_type: 'bigint'
+  default_value: 0
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "bigint",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "post_author",
+  {
+    data_type => "bigint",
+    default_value => 0,
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+  "post_date",
+  {
+    data_type => "datetime",
+    datetime_undef_if_invalid => 1,
+    default_value => "0000-00-00 00:00:00",
+    is_nullable => 0,
+  },
+  "post_date_gmt",
+  {
+    data_type => "datetime",
+    datetime_undef_if_invalid => 1,
+    default_value => "0000-00-00 00:00:00",
+    is_nullable => 0,
+  },
+  "post_content",
+  { data_type => "longtext", is_nullable => 0 },
+  "post_title",
+  { data_type => "text", is_nullable => 0 },
+  "post_excerpt",
+  { data_type => "text", is_nullable => 0 },
+  "post_status",
+  {
+    data_type => "varchar",
+    default_value => "publish",
+    is_nullable => 0,
+    size => 20,
+  },
+  "comment_status",
+  {
+    data_type => "varchar",
+    default_value => "open",
+    is_nullable => 0,
+    size => 20,
+  },
+  "ping_status",
+  {
+    data_type => "varchar",
+    default_value => "open",
+    is_nullable => 0,
+    size => 20,
+  },
+  "post_password",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "post_name",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 200 },
+  "to_ping",
+  { data_type => "text", is_nullable => 0 },
+  "pinged",
+  { data_type => "text", is_nullable => 0 },
+  "post_modified",
+  {
+    data_type => "datetime",
+    datetime_undef_if_invalid => 1,
+    default_value => "0000-00-00 00:00:00",
+    is_nullable => 0,
+  },
+  "post_modified_gmt",
+  {
+    data_type => "datetime",
+    datetime_undef_if_invalid => 1,
+    default_value => "0000-00-00 00:00:00",
+    is_nullable => 0,
+  },
+  "post_content_filtered",
+  { data_type => "longtext", is_nullable => 0 },
+  "post_parent",
+  {
+    data_type => "bigint",
+    default_value => 0,
+    extra => { unsigned => 1 },
+    is_nullable => 0,
+  },
+  "guid",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "menu_order",
+  { data_type => "integer", default_value => 0, is_nullable => 0 },
+  "post_type",
+  {
+    data_type => "varchar",
+    default_value => "post",
+    is_nullable => 0,
+    size => 20,
+  },
+  "post_mime_type",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 100 },
+  "comment_count",
+  { data_type => "bigint", default_value => 0, is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07046 @ 2018-07-15 12:07:23
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:nMn+6ADXb+wDaW22xfgTWA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
